8c923effd8e107b1c354cd8cba3581ed3f1e63e7
matthew <mashh@live.co.uk>
2024-01-02 12:20:04 +0000

    app.ts

---
+++ app.ts
@@ -0,0 +1,7 @@
+const apiRouter = require('./routes/api-router');
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use('/api', apiRouter)
+
+module.exports = app;
