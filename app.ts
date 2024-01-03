583a199cc6d6cac5aa2f30baa03fa88ec6d441a4
matthew <mashh@live.co.uk>
2024-01-03 15:11:43 +0000

    app.use cors

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import cors from 'cors';
 import apiRouter from './routes/api-router';
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRouter)
   
