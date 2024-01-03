ae82438f35c5096f4c6ca7db909a25a404aa9a42
matthew <mashh@live.co.uk>
2024-01-03 10:33:40 +0000

    add redeem router in api-router

--- routes/api-router.ts
+++ routes/api-router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import loyaltyRouter from './loyalty-router';
 import usersRouter from './users-router';
+import redeemRouter from './redeem-router';
 
 const apiRouter = Router();
 apiRouter.use('/loyalty_cards', loyaltyRouter);
 apiRouter.use('/users', usersRouter);
+apiRouter.use('/redeem', redeemRouter);
 
 export default apiRouter;
