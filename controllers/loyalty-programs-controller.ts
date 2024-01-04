04856bef261f1644865c2a21a8faf4b396feddcf
matthew <mashh@live.co.uk>
2024-01-04 16:14:54 +0000

    loyaly programs model/controller

--- controllers/loyalty-programs-controller.ts
+++ controllers/loyalty-programs-controller.ts
@@ -4,7 +4,7 @@ import { merchantLoyaltyPrograms } from '../models/loyalty-programs-model';
 export const getAllMerchantPrograms = (req: Request, res: Response, next: NextFunction) => {
     merchantLoyaltyPrograms(req)
     .then((data: any) => {
-        res.status(200).send({ loyalty_card: data[0] })
+        res.status(200).send({ loyalty_programs: data[0] })
     })
     .catch((err: any) => {
         next(err)
--- models/loyalty-programs-model.ts
+++ models/loyalty-programs-model.ts
@@ -3,7 +3,7 @@ import db from '../db/connection'
 export const merchantLoyaltyPrograms = (req: any) => {
     const { params } = req
         return db.query(`
-        SELECT * FROM loyalty_cards
+        SELECT * FROM loyalty_programs
         WHERE merchant_id = $1
         `, [params.merchant_id])
     .then((data: any) => {
