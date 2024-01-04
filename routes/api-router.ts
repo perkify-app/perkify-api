5447f1b4e5b36ab05133d4efcbd7af95317051b3
matthew <mashh@live.co.uk>
2024-01-04 15:21:49 +0000

    loyaltyRouter rename to loyalty-card-router

--- routes/api-router.ts
+++ routes/api-router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import loyaltyRouter from './loyalty-router';
+import loyaltyCardRouter from './loyalty-card-router';
 import merchantRouter from './merchant-router';
 import usersRouter from './users-router';
 import redeemRouter from './redeem-router';
@@ -7,7 +7,7 @@ import endpointsRouter from './endpoints-router';
 
 const apiRouter = Router();
 apiRouter.use('', endpointsRouter);
-apiRouter.use('/loyalty_cards', loyaltyRouter);
+apiRouter.use('/loyalty_cards', loyaltyCardRouter);
 apiRouter.use('/merchants', merchantRouter);
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/redeem', redeemRouter);
