af444f46f5e809acc1a82465cd8d2167f630702d
matthew <mashh@live.co.uk>
2024-01-05 11:52:01 +0000

    allow endpoint to be queried by user

--- models/loyalty-card-model.ts
+++ models/loyalty-card-model.ts
@@ -1,13 +1,14 @@
 import db from "../db/connection";
 
 export const allLoyaltyCards = (req: any) => {
-    let { sort_by='points', order='desc' } = req.query
+    let { sort_by='points', order='desc', user_id } = req.query
     if (sort_by.toLowerCase() !== 'points' && sort_by.toLowerCase() !== 'created_at') sort_by = 'id'
     if (order.toLowerCase() !== 'desc' && order.toLowerCase() !== 'asc') order = 'desc'
-    const queryStr = `
-    SELECT * FROM loyalty_cards
-    JOIN loyalty_programs ON loyalty_cards.loyalty_program_id = loyalty_programs.id
-    ORDER BY ${sort_by} ${order}`
+    let queryStr = `SELECT * FROM loyalty_cards`
+    queryStr += ` JOIN loyalty_programs ON loyalty_cards.loyalty_program_id = loyalty_programs.id`
+    if (user_id) queryStr += ` WHERE loyalty_cards.user_id = '${user_id}'`
+    queryStr += ` ORDER BY ${sort_by} ${order}`
+    console.log(queryStr)
     return db.query(queryStr)
     .then((data: any) => {
         return data.rows;
