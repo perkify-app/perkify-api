99e6bbfaf45a042c407286992b43bbb1a46163c3
matthew <mashh@live.co.uk>
2024-01-05 14:38:44 +0000

    models used id but referred to merchant_id

--- models/loyalty-card-model.ts
+++ models/loyalty-card-model.ts
@@ -1,14 +1,22 @@
 import db from "../db/connection";
 
 export const allLoyaltyCards = (req: any) => {
-    let { sort_by='points', order='desc', user_id } = req.query
+    let { params } = req
+    let { sort_by='points', order='desc', user_id, merchant_id } = req.query
     if (sort_by.toLowerCase() !== 'points' && sort_by.toLowerCase() !== 'created_at') sort_by = 'id'
     if (order.toLowerCase() !== 'desc' && order.toLowerCase() !== 'asc') order = 'desc'
     let queryStr = `SELECT * FROM loyalty_cards`
     queryStr += ` JOIN loyalty_programs ON loyalty_cards.loyalty_program_id = loyalty_programs.id`
     if (user_id) queryStr += ` WHERE loyalty_cards.user_id = '${user_id}'`
+    if (merchant_id || params.id) {
+        if (merchant_id) {
+            queryStr += ` WHERE loyalty_programs.merchant_id = '${merchant_id}'`
+        }
+        if (params.id) {
+            queryStr += ` WHERE loyalty_programs.merchant_id = '${params.id}'`
+        }
+    }
     queryStr += ` ORDER BY ${sort_by} ${order}`
-    console.log(queryStr)
     return db.query(queryStr)
     .then((data: any) => {
         return data.rows;
