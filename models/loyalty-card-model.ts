2943a6f7d17fc00022bb6f12a0d03ab806251327
matthew <mashh@live.co.uk>
2024-01-05 09:39:40 +0000

    loyalty_card joins loyalty_programs on same id

--- models/loyalty-card-model.ts
+++ models/loyalty-card-model.ts
@@ -9,9 +9,11 @@ export const allLoyaltyCards = () => {
 export const specificLoyaltyCard = (req: any) => {
     const { params } = req
         return db.query(`
-        SELECT * FROM loyalty_cards
-        WHERE id = $1
-        `, [params.loyalty_card_id])
+        SELECT *
+        FROM loyalty_cards
+        JOIN loyalty_programs ON loyalty_cards.loyalty_program_id = loyalty_programs.id
+        WHERE user_id = $1
+        `, [params.user_id])
     .then((data: any) => {
         return data.rows
     })
