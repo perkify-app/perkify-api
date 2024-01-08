0abf0abdfabbbde0c1f6552eb3248067024bd701
matthew <mashh@live.co.uk>
2024-01-08 10:03:34 +0000

    loyalty card model can also query by merchant_id

--- models/loyalty-card-model.ts
+++ models/loyalty-card-model.ts
@@ -7,7 +7,6 @@ export const allLoyaltyCards = (req: any) => {
     if (order.toLowerCase() !== 'desc' && order.toLowerCase() !== 'asc') order = 'desc'
     let queryStr = `SELECT * FROM loyalty_cards`
     queryStr += ` JOIN loyalty_programs ON loyalty_cards.loyalty_program_id = loyalty_programs.id`
-    console.log(user_id, merchant_id, params.id)
     if (user_id) queryStr += ` WHERE loyalty_cards.user_id = '${user_id}'`
     if (merchant_id || params.id) {
         if (merchant_id && user_id) {
