e34305cb3161c75533d77519893c48fae8f06e5b
matthew <mashh@live.co.uk>
2024-01-09 13:07:54 +0000

    tsc

---
+++ dist/controllers/error-controller.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handleFourOhFour = void 0;
+const path_1 = __importDefault(require("path"));
+const handleFourOhFour = (req, res, next) => {
+    res.status(400).sendFile(path_1.default.join(__dirname, '../assets', 'john-travolta-searching.gif'));
+};
+exports.handleFourOhFour = handleFourOhFour;
