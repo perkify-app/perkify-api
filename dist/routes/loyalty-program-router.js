640136c775d12dba4c74a43a7ce24161b9b7c3c1
matthew <mashh@live.co.uk>
2024-01-10 13:41:21 +0000

    new dist compile

--- dist/routes/loyalty-program-router.js
+++ dist/routes/loyalty-program-router.js
@@ -4,6 +4,9 @@ const express_1 = require("express");
 const loyaltyProgramRouter = (0, express_1.Router)();
 const loyalty_programs_controller_1 = require("../controllers/loyalty-programs-controller");
 loyaltyProgramRouter
+    .route('/')
+    .get(loyalty_programs_controller_1.getAllPrograms);
+loyaltyProgramRouter
     .route('/:id')
     .get(loyalty_programs_controller_1.getAllMerchantPrograms);
 exports.default = loyaltyProgramRouter;
