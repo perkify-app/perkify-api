7d32b307f39d3e8869fdd4bda6a530e6ed3f049e
matthew <mashh@live.co.uk>
2024-01-11 12:08:58 +0000

    alert query for redeem

--- dist/routes/endpoints-router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const endpointsRouter = (0, express_1.Router)();
-const endpoints_controller_1 = require("../controllers/endpoints-controller");
-endpointsRouter
-    .route('/')
-    .get(endpoints_controller_1.getEndpointsJson);
-exports.default = endpointsRouter;
